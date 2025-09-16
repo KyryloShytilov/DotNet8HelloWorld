d10c389d9f0554d06c847f1675baa6f519016c19
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-09-16 12:44:55 +0200

    Add print version

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine($".NET version: {RuntimeInformation.FrameworkDescription}");
     }
 }
